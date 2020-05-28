5200567c611fd37c2ae83ee3228c38fb9bf85f1d
Joshua Halls <joshuah40@googlemail.com>
2020-05-28 14:56:12 +0100

    functio

--- disinfo/functions.py
+++ disinfo/functions.py
@@ -15,36 +15,14 @@ def hello(name):
     print("Hello " + name)
     return
 
-    
+
 def date_range(x):
     early = min(x)
     late = max(x)
     return early, late, print(f"The latest date is is {late} and the earliest date is {early}")
 
-def get_subreddit_names(reddit_object, search_terms):
-    
-    reddit = reddit_object
-    
-    topics_dict = {  
-                        "subreddit": [] 
-                  }
-    
-    topic_list = search_terms
-    
-    for topic in topic_list:
-    
-        cont_subreddit = reddit.subreddit("all").search(topic)
-        
-        for submission in cont_subreddit:
-                topics_dict["subreddit"].append(submission.subreddit)
-        
-    data = pd.DataFrame(topics_dict)
-    
-    return data
-    
-
 def load_config(config_file = "config.yaml"):
-    
+
     config_yml = open(config_file)
     config = yaml.load(config_yml)
     return config
