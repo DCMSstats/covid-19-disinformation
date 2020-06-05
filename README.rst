d6c7c2c4737f303836cae502c7839c23cee5014f
Joshua Halls <joshuah40@googlemail.com>
2020-06-05 11:07:14 +0100

    docs

---
+++ README.rst
@@ -0,0 +1,7 @@
+ain Features
+-------------
+You define your mapping with your data as a ``GPTable``.
+
+You can define the format of your mapping with a ``Theme``, or simply use the default - gptheme.
+
+You ``write_workbook()`` to win.
--- disinfo/functions.py
+++ disinfo/functions.py
@@ -22,7 +22,20 @@ def date_range(x):
     return early, late, print(f"The latest date is is {late} and the earliest date is {early}")
 
 def load_config(config_file = "config.yaml"):
+    """
+    Loads the config file for the reddit pipeline
 
+    parameters
+    ----------
+    config_file: str
+        Name of the config file. Defaults config.ymal
+
+    returns
+    -------
+    Dictionary of config
+
+
+    """
     config_yml = open(config_file)
     config = yaml.load(config_yml)
     return config
