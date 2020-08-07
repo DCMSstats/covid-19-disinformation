2e6e20486434d765753e548e7808b926709c0f28
David Foster <31405412+foster999@users.noreply.github.com>
2020-08-07 16:16:56 +0100

    Add template API functions (#32)
    * Add template API functions
    
    * Fix doc types

---
+++ disinfo/api.py
@@ -0,0 +1,36 @@
+def update_subreddit_names(reddit_object, search_terms):
+    """
+    Get a list of subreddit names using a list of search terms. Append these to
+    the existing list of subreddits to collect.
+
+    Parameters
+    ----------
+    reddit_object : ?
+    search_terms : list
+    """
+    pass
+
+
+def update_submissions(reddit_object, subreddit_name)
+    """
+    Get a list the most recent 1000 submissions for a subreddit. Append these
+    to the existing list of submissions.
+
+    Parameters
+    ----------
+    reddit_object : ?
+    subreddit_names : str
+    """
+    pass
+
+
+def collect_new_comments(reddit_object, submission_id):
+    """
+    If new comments have been posted for a given submission, collect these.
+
+    Parameters
+    ----------
+    reddit_object : ?
+    subreddit_names : str
+    """
+    pass
